2038ca6f0ead2525184eaca109301bc6a310eb6c
Trevor Gerhardt <trevorgerhardt@gmail.com>
2013-05-28 16:28:31 -0400

    Dont add the element until first start

--- index.js
+++ index.js
@@ -3,53 +3,16 @@
  * Dependencies
  */
 
-var Spinner = require('spin.js');
-
-/**
- * Spinner options
- */
-
-var opts = {
-  lines: 13, // The number of lines to draw
-  length: 7, // The length of each line
-  width: 4, // The line thickness
-  radius: 10, // The radius of the inner circle
-  corners: 1, // Corner roundness (0..1)
-  rotate: 0, // The rotation offset
-  color: '#000', // #rgb or #rrggbb
-  speed: 1, // Rounds per second
-  trail: 60, // Afterglow percentage
-  shadow: false, // Whether to render a shadow
-  hwaccel: true, // Whether to use hardware acceleration
-  className: 'component-spinner-inner', // The CSS class to assign to the spinner
-  zIndex: 2e9, // The z-index (defaults to 2000000000)
-  top: 'auto', // Top position relative to parent in px
-  left: 'auto' // Left position relative to parent in px
-};
-
-/**
- * Create the element the spinner will use
- */
-
-var frag = document.createDocumentFragment()
-  , div = document.createElement('div');
-
-div.id = 'component-spinner';
-div.style.display = 'none';
-frag.appendChild(div);
-document.body.appendChild(frag);
-
-/**
- * Create the spinner
- */
-
-var spinner = new Spinner(opts);
+var Spinner = require('spin.js')
+  , spinner = null;
 
 /**
  * Start
  */
 
 function start(timeout) {
+  if (!spinner) spinner = createSpinner();
+
   spinner.spin(div);
 
   div.style['padding-top'] = div.style.height = window.innerHeight / 2;
@@ -67,9 +30,58 @@ function start(timeout) {
  */
 
 function stop() {
+  if (!spinner) spinner = createSpinner();
+
   spinner.stop();
 
   div.style.display = 'none';
+}
+
+/**
+ * Create spinner
+ */
+
+function createSpinner() {
+
+  /**
+   * Spinner options
+   */
+
+  var opts = {
+    lines: 13, // The number of lines to draw
+    length: 7, // The length of each line
+    width: 4, // The line thickness
+    radius: 10, // The radius of the inner circle
+    corners: 1, // Corner roundness (0..1)
+    rotate: 0, // The rotation offset
+    color: '#000', // #rgb or #rrggbb
+    speed: 1, // Rounds per second
+    trail: 60, // Afterglow percentage
+    shadow: false, // Whether to render a shadow
+    hwaccel: true, // Whether to use hardware acceleration
+    className: 'component-spinner-inner', // The CSS class to assign to the spinner
+    zIndex: 2e9, // The z-index (defaults to 2000000000)
+    top: 'auto', // Top position relative to parent in px
+    left: 'auto' // Left position relative to parent in px
+  };
+
+  /**
+   * Create the element the spinner will use
+   */
+
+  var frag = document.createDocumentFragment()
+    , div = document.createElement('div');
+
+  div.id = 'component-spinner';
+  div.style.display = 'none';
+  frag.appendChild(div);
+  document.body.appendChild(frag);
+
+  /**
+   * Create the spinner
+   */
+
+  return new Spinner(opts);
 }
 
 /**
